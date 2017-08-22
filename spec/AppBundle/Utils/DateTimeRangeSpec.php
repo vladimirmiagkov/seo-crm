bc05793488b386759ad2866600e4728c9c7f857c
vlad82 <impulsvideo@yandex.ru>
2017-08-22 08:39:44 +0300

    add DateTimeRange support class

---
+++ spec/AppBundle/Utils/DateTimeRangeSpec.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace spec\AppBundle\Utils;
+
+use AppBundle\Utils\DateTimeRange;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class DateTimeRangeSpec extends ObjectBehavior
+{
+    // Init ------------------------------------------------------------------------------------------------------------
+
+    function let()
+    {
+        $this->beConstructedWith(null, null, 'now -1 month', 'now');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(DateTimeRange::class);
+    }
+
+    function it_should_has_correct_state_values_after_initialization()
+    {
+        $this->getStart()->shouldReturnAnInstanceOf('DateTime');
+        $this->getEnd()->shouldReturnAnInstanceOf('DateTime');
+    }
+
+    function it_should_correctly_initialized_from_unixtimestamp()
+    {
+        $this->beConstructedWith('1503377630', '1503377639');
+        $this->shouldHaveType(DateTimeRange::class);
+    }
+
+    function it_should_correctly_initialized_from_string()
+    {
+        $this->beConstructedWith('now -1 month', 'now');
+        $this->shouldHaveType(DateTimeRange::class);
+    }
+
+    function it_should_correctly_initialized_from_datetime_php_object()
+    {
+        $this->beConstructedWith((new \DateTime()), (new \DateTime()));
+        $this->shouldHaveType(DateTimeRange::class);
+    }
+
+    function it_should_throw_exception_during_instantiation_with_incorrect_input_values()
+    {
+        $this->beConstructedWith('hm...', 'hm...');
+        $this->shouldThrow('\Exception')->duringInstantiation();
+    }
+
+    function it_should_throw_exception_during_instantiation_with_null_default_values()
+    {
+        $this->beConstructedWith(null, null, null, null);
+        $this->shouldThrow('\InvalidArgumentException')->duringInstantiation();
+    }
+
+    // Job -------------------------------------------------------------------------------------------------------------
+
+    function it_should_correctly_expand_range_to_full_day()
+    {
+        $expandedRange = $this->expandRangeToFullDay();
+        $expandedRange->shouldReturnAnInstanceOf(DateTimeRange::class);
+        $expandedRange->getStart()->format('H:i:s')->shouldBe('00:00:00');
+        $expandedRange->getEnd()->format('H:i:s')->shouldBe('23:59:59');
+    }
+
+    // Range positive / negative ---------------------------------------------------------------------------------------
+
+    function it_should_has_positive_range()
+    {
+        $this->beConstructedWith('now -1 month', 'now');
+        $this->isRangePositive()->shouldBe(true);
+    }
+
+    function it_should_has_positive_range_on_equal_start_and_end_points()
+    {
+        $this->beConstructedWith('now', 'now');
+        $this->isRangePositive()->shouldBe(true);
+    }
+
+    function it_should_has_negative_range()
+    {
+        $this->beConstructedWith('now', 'now -1 month');
+        $this->isRangePositive()->shouldBe(false);
+    }
+
+    function it_should_be_able_to_be_transformed_to_negative_range()
+    {
+        $this->beConstructedWith('now -1 month', 'now');
+        $transfomedRange = $this->makeRangeNegative();
+        $transfomedRange->isRangePositive()->shouldBe(false);
+    }
+
+    function it_should_be_able_to_be_transformed_to_positive_range()
+    {
+        $this->beConstructedWith('now', 'now -1 month');
+        $transfomedRange = $this->makeRangePositive();
+        $transfomedRange->isRangePositive()->shouldBe(true);
+    }
+
+    // Generate sequence -----------------------------------------------------------------------------------------------
+
+    function it_should_be_able_to_generate_datetime_sequence__day_by_day()
+    {
+        $this->beConstructedWith('now -1 month', 'now');
+        $this->generateSequence('P1D')->shouldBeArray();
+    }
+}
---
+++ src/AppBundle/Utils/DateTimeRange.php
@@ -0,0 +1,189 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Utils;
+
+/**
+ * Support class for work with datetime range.
+ * Datetime range: from "start" datetime point to "end" datetime point.
+ */
+class DateTimeRange
+{
+    /** @var \DateTime */
+    private $start;
+    /** @var \DateTime */
+    private $end;
+
+    /**
+     * Create date time range.
+     *
+     * @param null|string|\DateTime $start
+     * @param null|string|\DateTime $end
+     * @param string                $startDefault Default value for "start"
+     * @param string                $endDefault   Default value for "end"
+     */
+    public function __construct($start = null, $end = null, $startDefault = 'now -1 month', $endDefault = 'now')
+    {
+        if (empty($startDefault)) {
+            throw new \InvalidArgumentException('$startDefault cannot be empty.');
+        }
+        if (empty($endDefault)) {
+            throw new \InvalidArgumentException('$endDefault cannot be empty.');
+        }
+
+        if (empty($start)) {
+            $this->setStart(new \DateTime($startDefault));
+        } elseif (\is_numeric($start)) { // Unix timestamp representstion of date time.
+            $this->setStart((new \DateTime())->setTimestamp((int)$start));
+        } elseif (\is_string($start)) { // String representstion of date time.
+            $start = trim($start);
+            $this->setStart((new \DateTime($start)));
+        } elseif ($start instanceof \DateTime) { // DateTime representstion of date time.
+            $this->setStart($start);
+        } else {
+            throw new \InvalidArgumentException('Unsupported $start datetime format.');
+        }
+
+        if (empty($end)) {
+            $this->setEnd(new \DateTime($endDefault));
+        } elseif (\is_numeric($end)) { // Unix timestamp representstion of date time.
+            $this->setEnd((new \DateTime())->setTimestamp((int)$end));
+        } elseif (\is_string($end)) { // String representstion of date time.
+            $end = trim($end);
+            $this->setEnd((new \DateTime($end)));
+        } elseif ($end instanceof \DateTime) { // DateTime representstion of date time.
+            $this->setEnd($end);
+        } else {
+            throw new \InvalidArgumentException('Unsupported $end datetime format.');
+        }
+    }
+
+    /**
+     * Expand time range:
+     *   "start" set time to 00:00:00
+     *   "end"   set time to 23:59:59
+     *
+     * @return $this
+     */
+    public function expandRangeToFullDay()
+    {
+        $this->setStart(($this->getStart())->setTime(0, 0, 0));
+        $this->setEnd(($this->getEnd())->setTime(23, 59, 59));
+        return $this;
+    }
+
+    /**
+     * Determine: is date time range positive(from lower to higher)?
+     *   "lower"  like '2017-01-01'
+     *   "higher" like '2018-01-01'
+     *
+     * @return bool true = from lower to higher (or start == end); false = from higher to lower
+     */
+    public function isRangePositive()
+    {
+        return (bool)($this->getStart()->getTimestamp() <= $this->getEnd()->getTimestamp());
+    }
+
+    /**
+     * Swap start and end points if needed.
+     * Negative: from "higher" to "lower"
+     *
+     * @return $this
+     */
+    public function makeRangeNegative()
+    {
+        if ($this->isRangePositive()) {
+            $this->swapRange();
+        }
+        return $this;
+    }
+
+    /**
+     * Swap start and end points if needed.
+     * Positive: from "lower" to "higher"
+     *
+     * @return $this
+     */
+    public function makeRangePositive()
+    {
+        if (!$this->isRangePositive()) {
+            $this->swapRange();
+        }
+        return $this;
+    }
+
+    /**
+     * Generate sequence like:
+     *
+     * @param string $interval
+     * @return \DateTime[]|null
+     */
+    public function generateSequence(string $interval)
+    {
+        if (empty($interval)) {
+            throw new \InvalidArgumentException('$interval can not be empty');
+        }
+
+        $result = null;
+
+        $start = $this->getStart();
+        $end = $this->getEnd();
+        $interval = new \DateInterval($interval);
+
+        $dates = new \DatePeriod($start, $interval, $end);
+        if (!empty($dates)) {
+            foreach ($dates as $date) {
+                $result[] = $date;
+            }
+        }
+
+        return $result;
+    }
+
+    // Getters / setters -----------------------------------------------------------------------------------------------
+
+    /**
+     * @return \DateTime
+     */
+    public function getStart(): \DateTime
+    {
+        return clone($this->start);
+    }
+
+    /**
+     * @param \DateTime $start
+     * @return $this
+     */
+    private function setStart(\DateTime $start)
+    {
+        $this->start = clone($start);
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEnd(): \DateTime
+    {
+        return clone($this->end);
+    }
+
+    /**
+     * @param \DateTime $end
+     * @return $this
+     */
+    private function setEnd(\DateTime $end)
+    {
+        $this->end = clone($end);
+        return $this;
+    }
+
+    // Support methods -------------------------------------------------------------------------------------------------
+
+    private function swapRange()
+    {
+        $tmp = $this->getEnd();
+        $this->setEnd(clone($this->getStart()));
+        $this->setStart($tmp);
+    }
+}
